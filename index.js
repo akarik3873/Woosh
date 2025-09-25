b5265b0bfb7c4b3076367180f8a2068d01688413
akarik3873 <akarik3873@gmail.com>
2025-09-24 20:08:04 -0700

    did some ground work on some web pages and did a basic nav bar, gonna go in later for the react version to replace some stuff btu this should be dece for learning and teaching

---
+++ index.js
@@ -0,0 +1,8 @@
+const signUp = document.getElementbyId(SignUp)
+const logIn = document.getElementbyId(LogIn)
+button.addEventListener('click', signUp)
+button.addEventListener('click', logIn)
+function signUp() {
+
+}
+logIn.onclick
